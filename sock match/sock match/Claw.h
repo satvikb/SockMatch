b646b93134b84d92e97cbe3819bd0a90f2ecc90e
satvikb <borrasatvik@gmail.com>
2017-06-19 19:18:45 -0400

    added claw, testing from Xcode

---
+++ sock match/sock match/Claw.h
@@ -0,0 +1,18 @@
+//
+//  Claw.h
+//  sock match
+//
+//  Created by Satvik Borra on 6/19/17.
+//  Copyright © 2017 sborra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Sock.h"
+
+@interface Claw : UIView {
+    
+}
+
+-(id) initClawWithFrame:(CGRect)frame;
+
+@end
---
+++ sock match/sock match/Claw.m
@@ -0,0 +1,28 @@
+//
+//  Claw.m
+//  sock match
+//
+//  Created by Satvik Borra on 6/19/17.
+//  Copyright © 2017 sborra. All rights reserved.
+//
+
+#import "Claw.h"
+
+@implementation Claw
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+-(id) initClawWithSock:(Sock*)sockPackage {
+//    self = [super initWithFrame:frame];
+//    [self initWithFrame:frame];
+    
+    return self;
+}
+
+@end
